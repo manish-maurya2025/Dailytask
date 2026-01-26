f2d2296de2ee6d5b71de009341e45ae9391a28b7
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-01-26 23:36:28 +0530

    This is areacircle program

---
+++ AreaCircle.java
@@ -0,0 +1,14 @@
+
+    class AreaCircle {
+
+    static void findArea(double r) {
+        double area = Math.PI * r * r;
+        System.out.println("Area of Circle = " + area);
+    }
+
+    public static void main(String[] args) {
+        findArea(5);
+    }
+}
+
+
