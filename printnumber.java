b53e3dd139a068fc89a78ff03b78cdc7279ce5cb
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-01-28 23:54:39 +0530

    printnumber

---
+++ printnumber.java
@@ -0,0 +1,16 @@
+// for loop
+public class printnumber{
+    public static void main(String[] args) {
+    //  for (int i = 1; i <= 10; i++) {
+    //         System.out.println(i);
+    //  }
+    //    while loop
+         int i = 1;
+         while (i<=10){
+            System.out.println(i);
+            i++;
+         }
+          
+        }
+    }
+
