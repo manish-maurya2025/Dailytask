008b0fee83efa572dfc0323732d007d6c88ad0c6
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-02-10 23:43:46 +0530

    InsertDeleteLinklist

---
+++ Deletelinklist.java
@@ -0,0 +1,92 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void deleteFromBeginning() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;
+    }
+
+
+    void deleteFromEnd() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class Deletelinklist {
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+
+        System.out.println("Original List:");
+        list.display();
+
+        list.deleteFromBeginning();
+        System.out.println("After deleting from beginning:");
+        list.display();
+
+        list.deleteFromEnd();
+        System.out.println("After deleting from end:");
+        list.display();
+
+       
+    }
+}
+
+
