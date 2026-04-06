61c51bf4367d7795e4c33a202230674fcc61a858
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-04-06 23:59:40 +0530

    Bubblesorts

---
+++ BubbleSorts.java
@@ -0,0 +1,25 @@
+  class BubbleSorts {
+    public static void main(String[] args) {
+
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        int n = arr.length;
+        int temp = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
